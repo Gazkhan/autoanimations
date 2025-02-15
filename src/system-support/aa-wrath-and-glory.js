ae0de318d619c053dcda07d3b66b6d796dc8f6cb
Jules-Bens-Aa <73002638+Jules-Bens-Aa@users.noreply.github.com>
2025-02-15 07:24:24 +0100

    first tentative fix wrath and glory

--- src/system-support/aa-wrath-and-glory.js
+++ src/system-support/aa-wrath-and-glory.js
@@ -6,13 +6,13 @@ export function systemHooks() {
     Hooks.on("createChatMessage", async (msg) => {
         if (msg.user.id !== game.user.id) { return };
         const systemName = 'wrath-and-glory';
-        let success = msg.flags[systemName].testData.result.isSuccess;
-        if (success == true && msg.flags[systemName].testData.result.damage.dice.length !== 0) { return };
+        let success = msg.flags[systemName].itemData.result.isSuccess;
+        if (success == true && msg.flags[systemName].itemData.result.damage.dice.length !== 0) { return };
 
         let compiledData = await getRequiredData({
-            actorId: msg.speaker.actor ?? msg.flags[systemName].testData.context.speaker.actor,
-            targets: compileTargets(msg.flags[systemName].testData.context.targets),
-            itemUuid: msg.flags[systemName].testData.testData.itemId,
+            actorId: msg.speaker.actor ?? msg.flags[systemName].itemData.context.speaker.actor,
+            targets: compileTargets(msg.flags[systemName].itemData.context.targets),
+            itemUuid: msg.flags[systemName].itemData.testData.itemId,
             workflow: msg,
         })
         if (!compiledData.item) { return; }
